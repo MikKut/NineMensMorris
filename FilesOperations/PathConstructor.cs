1dbf3b560b73a3f89cdde9016f3286e989451230
MikKut <misha.kutny1@gmail.com>
2022-07-06 13:23:21 +0300

    Добавьте файлы проекта.

---
+++ FilesOperations/PathConstructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NineMensMorris.FilesOperations
+{
+    internal static class Paths
+    {
+        public readonly static string
+            pathFromBootDirectoryToStartProjDir =
+           Directory.GetCurrentDirectory().Replace(PathFromStartProjFolderToSolutionToExeFile, ""),
+            pathOfTheCounter =
+           pathFromBootDirectoryToStartProjDir + @$"{PathFromStartProjFolderToSaves}\{NameOfTheFileWithCounter}",
+            pathOfTheNames =
+           pathFromBootDirectoryToStartProjDir + @$"{PathFromStartProjFolderToSaves}\{NameOfTheFileWithNames}";
+        public const string
+            NameOfTheFileWithNames = "Names.txt",
+            NameOfTheFileWithCounter = "Counter.txt",
+            PathFromStartProjFolderToSolutionToExeFile = @"\bin\Debug\net6.0-windows",
+            PathFromStartProjFolderToSaves = @"\FilesOperations\Saves";
+
+    }
+
+}
